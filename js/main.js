1826991a3674abea0a363d4e4d6c57d49f0dca3f
MykeArt <mickael.sorhaindo@gmail.com>
2019-04-26 18:44:10 +0200

    Card-Flip Css/Js

--- js/main.js
+++ js/main.js
@@ -3,25 +3,32 @@
 const card = document.querySelectorAll('.card')
 const frontCard = document.querySelectorAll('.card .card-container .front-face')
 const backCard = document.querySelectorAll('.card .card-container .back-face')
-const button = document.querySelectorAll('button')
+const button = document.querySelectorAll('.sect button')
+const buttonSection = document.querySelector('.sect')
+const plus = document.querySelector('.plus')
 
+// Boucle pour cartes
 for(let i = 0; i < frontCard.length; i++)
 {
+	// Déclencher l'animation
 	card[i].onmouseenter = () => 
 	{
 		frontCard[i].className = "front-face frontCard"
 		backCard[i].className = "back-face backCard"
 	}
-
+	// Stopper l'animation
 	card[i].onmouseleave = () => 
 	{
 		frontCard[i].className = "front-face"
 		backCard[i].className = "back-face"
 	}
-
+	// Animation au click 180°
 	button[i].onclick = () => 
-		{
-			frontCard[i].classList.toggle("front-reverse")
-			backCard[i].classList.toggle("back-reverse")
-		}
+	{
+		frontCard[i].classList.toggle("front-reverse")
+		backCard[i].classList.toggle("back-reverse")
+	}
+
 }
+// Bouton plus pour cacher 
+plus.onclick = () => buttonSection.style.opacity === '0' ? buttonSection.style.opacity = '100' : buttonSection.style.opacity = '0'
